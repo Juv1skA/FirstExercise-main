67805b42dae8a268c24b023db025779ee9c8aeda
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-01-08 12:00:44 +0200

    added current year display

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
 });
+
+// displays current year 
+document.getElementById("currentYear").innerText = new Date().getFullYear();
+
