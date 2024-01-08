6daa8a7ae018a1c746d0a3aca11a7d7e709f850a
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-01-08 12:01:08 +0200

    hides description on button press

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
+    ToggleDescription();
 });
 
 // displays current year 
 document.getElementById("currentYear").innerText = new Date().getFullYear();
 
+// hides description after button press
+function ToggleDescription() {
+    var paragraph = document.getElementById("description");
+    paragraph.style.display = (paragraph.style.display === "none" || paragraph.style.display === "") ? "block" : "none";
+}
